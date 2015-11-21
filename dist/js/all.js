1ecc6ef3413be7e3315d80f3dfb04616c284855d
Michał Woźniczak <michal.wozniczak@gmail.com>
2015-11-21 11:43:49 +0100

    Formatting fixes (mostly for printing)

--- dist/js/all.js
+++ dist/js/all.js
@@ -93,7 +93,7 @@ window.onload = function() {
 			img.src = "assets/" + size + "-" + j + ".png";
 			img.onload = function() {
 				images.loaded++;
-			}
+			};
 			images.items[size].push(img);
 		}
 	}
@@ -118,7 +118,7 @@ window.onload = function() {
 		var chanceY = {
 			min: -parseInt(instance.img.height * 0.75),
 			max: canvas.height + parseInt(instance.img.height * 0.75)
-		}
+		};
 		instance.placementX = chance.integer(chanceX);
 		instance.placementY = chance.integer(chanceY);
 
@@ -148,7 +148,7 @@ window.onload = function() {
 			for (var i in placements[size]) {
 				var imgObj = placements[size][i];
 				context.drawImage(imgObj.img, imgObj.placementX, imgObj.placementY);
-				imgObj.placementX = imgObj.placementX - imgObj.speed;
+				imgObj.placementX = (imgObj.placementX - imgObj.speed);
 				if (shouldFadeout(imgObj)) {
 					placements[size][i] = makeImg(size, true);
 				}
